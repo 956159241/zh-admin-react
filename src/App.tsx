4e1e29bc96b06cb10d6dd4521f2197ac53626786
张中华 <956159241@qq.com>
2023-01-09 11:11:50 +0800

    添加redux

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,17 @@ import { Provider } from 'react-redux'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
 import Router from './router'
+import store from '../src/store/index.js'
 
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-  </ConfigProvider>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </Provider>
+      </ConfigProvider>
   );
 }
 
