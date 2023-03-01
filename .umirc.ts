b1987348d38f1cf44a20bf795ad29a8cb9d65fe6
张中华 <956159241@qq.com>
2023-03-01 10:39:33 +0800

    update config by the way.

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
+  targets: { chrome: 80 }, // Umi 4 默认不支持 IE ，编译兼容目标 targets 为 chrome: 80
   antd: {},
   access: {},
   model: {},
@@ -31,4 +32,11 @@ export default defineConfig({
     },
   ],
   npmClient: 'pnpm',
+
+  // Umi 4 自带提供一个 legacy 配置用于构建降级（使用限制等详见 legacy: https://umijs.org/docs/api/config#legacy）：
+  legacy: {},
+
+  // Umi 4 默认使用现代构建工具，产物生成至 es6 ，如果你有要打包为 es5 产物的考量，请调整配置：
+  // jsMinifier: 'terser',
+  // cssMinifier: 'cssnano'
 });
